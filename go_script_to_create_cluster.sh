c6a2e7dadf9bf515b943850dcd58b31bd9164ce3
git4impatient <lurie@lurie.biz>
2016-10-06 10:46:47 -0400

    Update go_script_to_create_cluster.sh

--- go_script_to_create_cluster.sh
+++ go_script_to_create_cluster.sh
@@ -3,7 +3,14 @@
 # 
 # 
 #
-sudo yum -y install java-1.8.0-openjdk-devel.x86_64 
+yum install -y wget
+yum install -y git
+yum remove *openjdk*
+cd /tmp
+wget http://archive.cloudera.com/director/redhat/7/x86_64/director/2.1.0/RPMS/x86_64/oracle-j2sdk1.8-1.8.0+update60-1.x86_64.rpm
+rpm -ivh /tmp/oracle-j2sdk1.8-1.8.0+update60-1.x86_64.rpm
+
+ 
 # install cloudera director
 cd /etc/yum.repos.d/
 sudo wget "http://archive.cloudera.com/director/redhat/6/x86_64/director/cloudera-director.repo"
